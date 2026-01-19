575d9df80f4d00edcd0d08e53fdec9e454eca18f
riosst100 <riosst29@gmail.com>
2026-01-19 09:41:38 +0700

    styling homepage

--- app/components/HomeHeader.vue
+++ app/components/HomeHeader.vue
@@ -6,7 +6,7 @@ import { Icon } from '@iconify/vue'
   <header class="bg-white sticky top-0 z-10 border-b">
     <div class="flex items-center justify-between px-4 h-14">
       <span class="font-bold text-indigo-600 text-lg"><img
-        src="/logo/logo_text.svg"
+        src="/logo/logo-text.svg"
         alt="EventHub"
         class="w-auto"
         style="height:20px"
