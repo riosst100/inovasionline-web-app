4dab6556d7e9ff30c303071da5015bed13700f91
riosst100 <riosst29@gmail.com>
2026-01-20 23:27:18 +0700

    update

--- app/components/HomeCategories.vue
+++ app/components/HomeCategories.vue
@@ -1,33 +1,54 @@
 <template>
-  <CategorySkeleton v-if="pending" />
-  <template v-else>
-    <section class="px-4 mt-4">
-      <div class="grid grid-cols-4 gap-4">
-        <NuxtLink
-          v-for="category in categories"
-          :key="category.id"
-          :to="`/kategori/${category.slug}`"
-          class="flex flex-col items-center gap-2"
-        >
-          <div
-            class="w-14 h-14 rounded-full category-item flex items-center justify-center"
-          >
-            <img
-              v-if="category.icon"
-              :src="`/categories/${category.icon}.webp`"
-              :alt="category.name"
-              class="w-7 h-7"
-            />
-          </div>
+  <ClientOnly>
+    <template #default>
+      <section class="px-4 mt-4">
+        <CategorySkeleton v-if="pending" />
 
-          <span class="text-xs text-gray-700 text-center">
-            {{ category.name }}
-          </span>
-        </NuxtLink>
-      </div>
-    </section>
-  </template>
+        <div v-else class="grid grid-cols-4 gap-4">
+          <NuxtLink
+            v-for="category in categories"
+            :key="category.id"
+            :to="`/kategori/${category.slug}`"
+            class="flex flex-col items-center gap-2"
+          >
+            <div
+              class="w-14 h-14 rounded-full category-item flex items-center justify-center"
+            >
+              <img
+                v-if="category.icon"
+                :src="`/categories/${category.icon}.webp`"
+                :alt="category.name"
+                class="w-7 h-7"
+              />
+            </div>
+
+            <span class="text-xs text-gray-700 text-center">
+              {{ category.name }}
+            </span>
+          </NuxtLink>
+        </div>
+      </section>
+    </template>
+
+    <!-- optional fallback -->
+    <template #fallback>
+      <section class="px-4 mt-4">
+        <CategorySkeleton />
+      </section>
+    </template>
+  </ClientOnly>
 </template>
+
+<script setup>
+const { data: categories, pending } = useAsyncData(
+  'categories-homepage',
+  () => $fetch('/api/categories/homepage'),
+  {
+    server: false,
+    default: () => [],
+  }
+)
+</script>
 
 <style scoped>
 .category-item {
@@ -35,14 +56,3 @@
   border: 1px solid #e5e7eb;
 }
 </style>
-<script setup>
-  const { data: categories } = await useAsyncData(
-  'categories-homepage',
-  () => $fetch('/api/categories/homepage'),
-  {
-    server: false,
-    lazy: false,
-    default: () => null,
-  }
-)
-</script>
