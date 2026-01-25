fc76f6a9b6a061fd3633f5f862a1c73addb63c1c
riosst100 <riosst29@gmail.com>
2026-01-25 12:44:54 +0700

    update

--- app/components/HomeHeader.vue
+++ app/components/HomeHeader.vue
@@ -4,7 +4,7 @@ import { Icon } from '@iconify/vue'
 
 <template>
   <header class="bg-white sticky top-0 z-10 border-b">
-    <div class="flex items-center justify-between px-4 h-[3rem]">
+    <div class="flex items-center justify-between px-4 h-[3.2rem]">
       <span class="font-bold text-indigo-600 text-lg">
         <img
             src="/logo/logo_text_v2.svg"
@@ -14,8 +14,8 @@ import { Icon } from '@iconify/vue'
         />
     </span>
     <div class="flex items-center gap-[15px] text-gray-600">
-        <a href="/cart" class="header-icon"><Icon icon="lineicons:cart-1" width="28" height="28" /></a>
-        <a href="/messages" class="header-icon"><Icon icon="token:chat" width="28" height="28" /></a>
+        <a href="/cart" class="header-icon"><Icon icon="lineicons:cart-1" width="29" height="29" /></a>
+        <a href="/messages" class="header-icon"><Icon icon="token:chat" width="29" height="29" /></a>
       </div>
     </div>
   </header>
