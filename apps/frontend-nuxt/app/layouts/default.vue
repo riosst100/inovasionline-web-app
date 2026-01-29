21dd19cd7e19ffbdf032ebaedc7f98ec55fb1a78
riosst100 <riosst29@gmail.com>
2026-01-29 16:45:50 +0700

    update

--- apps/frontend-nuxt/app/layouts/default.vue
+++ apps/frontend-nuxt/app/layouts/default.vue
@@ -5,9 +5,11 @@
     </main>
   <HomeFooter />
   <BottomNavbar />
+  <SpeedInsights />
 </template>
 
 <script setup>
   import BottomNavbar from '~/components/BottomNavbar.vue';
   import HomeHeader from '~/components/HomeHeader.vue';
+  import { SpeedInsights } from '@vercel/speed-insights/vue';
 </script>
