f6287e281fed833dbaf04fab61627548ed7a07ae
riosst100 <riosst29@gmail.com>
2026-01-30 15:05:50 +0700

    add analytics

--- apps/frontend-nuxt/app/layouts/default.vue
+++ apps/frontend-nuxt/app/layouts/default.vue
@@ -6,10 +6,12 @@
   <HomeFooter />
   <BottomNavbar />
   <SpeedInsights />
+  <Analytics />
 </template>
 
 <script setup>
   import BottomNavbar from '~/components/BottomNavbar.vue';
   import HomeHeader from '~/components/HomeHeader.vue';
   import { SpeedInsights } from '@vercel/speed-insights/vue';
+  import { Analytics } from '@vercel/analytics/nuxt'
 </script>
