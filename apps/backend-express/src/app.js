8826b0331f9df31ef545865e59ce21dfb7754ee1
riosst100 <riosst29@gmail.com>
2026-01-30 11:44:00 +0700

    update

--- apps/backend-express/src/app.js
+++ apps/backend-express/src/app.js
@@ -1,8 +1,11 @@
 import express from 'express'
+import compression from 'compression'
 import cors from 'cors'
 import homepageRoutes from './routes/homepage.route.js'
 
 const app = express()
+
+app.use(compression())
 
 app.use('/api', cors(), express.json(), homepageRoutes)
 
