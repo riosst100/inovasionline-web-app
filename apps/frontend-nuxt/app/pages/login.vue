13b0c2c5e59258c5a82e57f8dd9931196053dc3e
riosst100 <riosst29@gmail.com>
2026-01-30 19:18:56 +0700

    update

--- apps/frontend-nuxt/app/pages/login.vue
+++ apps/frontend-nuxt/app/pages/login.vue
@@ -1,6 +1,45 @@
-<script setup>
+<script setup lang="ts">
+definePageMeta({
+  ssr: false
+})
+
+const auth = useAuth()
+
+watchEffect(() => {
+  if (auth.accessToken.value) {
+    navigateTo('/', { replace: true })
+  }
+})
+
+const email = ref('')
+const password = ref('')
+const error = ref('')
+
+const submit = async () => {
+  try {
+    await auth.login(email.value, password.value)
+  } catch {
+    error.value = 'Login gagal'
+  }
+}
+
+const config = useRuntimeConfig()
+
+const loginGoogle = () => {
+  window.location.href = `${config.public.backendUrl}/auth/google`
+}
 </script>
 
 <template>
-  <GoogleLogin />
+  <form @submit.prevent="submit">
+    <h1>Login</h1>
+    <!-- <input v-model="email" placeholder="Email" />
+    <input v-model="password" type="password" />
+    <button>Login</button> -->
+    <!-- <p v-if="error">{{ error }}</p> -->
+
+    <button @click="loginGoogle">
+      Login dengan Google
+    </button>
+  </form>
 </template>
