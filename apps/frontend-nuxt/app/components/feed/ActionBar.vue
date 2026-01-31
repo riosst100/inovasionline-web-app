d597bec35f16dc298fe5ceda74dcabf7c1ea812f
riosst100 <riosst29@gmail.com>
2026-01-31 18:35:14 +0700

    update

--- apps/frontend-nuxt/app/components/feed/ActionBar.vue
+++ apps/frontend-nuxt/app/components/feed/ActionBar.vue
@@ -1,17 +1,48 @@
 <template>
   <div class="flex justify-around text-sm text-gray-600 border-t pt-3 mt-3">
-    <button @click="likes++">👍 Like {{ likes }}</button>
-    <button>💬 Comment</button>
-    <button @click="shares++">↗ Share {{ shares }}</button>
+    <button
+      class="flex items-center gap-1 hover:text-blue-500"
+      @click="likes++"
+    >
+      <Icon icon="stash:thumb-up" class="post-icon" />
+      {{ likes }}
+    </button>
+
+    <button class="flex items-center gap-1 hover:text-blue-500">
+      <Icon icon="iconamoon:comment-light" class="post-icon" />
+      {{ shares }}
+    </button>
+
+    <button
+      class="flex items-center gap-1 hover:text-blue-500"
+      @click="shares++"
+    >
+      <Icon icon="ph:share-fat" class="post-icon" />
+      {{ shares }}
+    </button>
   </div>
 </template>
 
+<style scoped>
+  .post-icon {
+    width: 22px;
+    height: 22px;
+  }
+</style>
+
 <script setup>
 import { ref } from 'vue'
+import { Icon } from '@iconify/vue'
 
 const props = defineProps({
-  likes: Number,
-  shares: Number,
+  likes: {
+    type: Number,
+    default: 0
+  },
+  shares: {
+    type: Number,
+    default: 0
+  }
 })
 
 const likes = ref(props.likes)
