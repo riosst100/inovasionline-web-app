3c9810bc0a046db77a7e2267dbcd82dc6a7c6a0d
riosst100 <riosst29@gmail.com>
2026-02-01 09:13:39 +0700

    update

--- apps/frontend-nuxt/app/pages/logout-loading.vue
+++ apps/frontend-nuxt/app/pages/logout-loading.vue
@@ -20,3 +20,12 @@ onMounted(() => {
   })
 })
 </script>
+
+<template>
+  <div class="min-h-screen flex flex-col items-center justify-center bg-white">
+    <div
+      class="w-8 h-8 border-4 border-gray-300 border-t-gray-700 rounded-full animate-spin mb-3"
+    />
+    <p class="text-sm text-gray-500">Sedang logout...</p>
+  </div>
+</template>
