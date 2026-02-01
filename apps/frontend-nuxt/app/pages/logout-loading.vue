08042502f32d9f17fe5ab3711c44ac4da01a35c4
riosst100 <riosst29@gmail.com>
2026-02-01 09:30:46 +0700

    update

--- apps/frontend-nuxt/app/pages/logout-loading.vue
+++ apps/frontend-nuxt/app/pages/logout-loading.vue
@@ -26,6 +26,6 @@ onMounted(() => {
     <div
       class="w-8 h-8 border-4 border-gray-300 border-t-gray-700 rounded-full animate-spin mb-3"
     />
-    <p class="text-sm text-gray-500">Sedang logout...</p>
+    <p class="text-sm text-gray-500">Sedang Proses Keluar...</p>
   </div>
 </template>
